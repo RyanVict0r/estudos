198d49d82eb3e3c73ea979281009a1c6c0e58b57
Ryan Victor <87503898+RyanVict0r@users.noreply.github.com>
2022-11-12 11:43:05 -0300

    Add files via upload

---
+++ estudos/operadores/src/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+    public static void main(String[] args){
+        String NomeUm = "Ryan";
+        String NomeDois = new String("Ryam");
+
+        System.out.println(NomeUm.equals(NomeDois));
+      
+
+        int numero1 = 3;
+        int numero2 = 3;
+
+        boolean SimNao = numero1 == numero2;
+        System.out.println("NumeroUm é igual NumeroDois? " + SimNao);
+    }
+}
